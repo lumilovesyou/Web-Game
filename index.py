b5c708786db7bfbdb7c58c1ed795549bbdf6ec4f
lumilovesyou <felis.araneae@gmail.com>
2026-08-24 12:25:25 -0500

    Basic Program

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-print("hello world!")
+from pyray import *
+
+init_window(800, 450, "Wow")
+set_target_fps(60)
+
+while not window_should_close():
+    begin_drawing()
+    clear_background(RAYWHITE)
+    draw_text("Hello world!", 190, 200, 20, BLACK)
+    end_drawing()
+    
+close_window()
