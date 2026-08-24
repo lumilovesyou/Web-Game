9bd1e9a84c004b4a8fd7c51d432a86b6ae694c23
lumilovesyou <felis.araneae@gmail.com>
2026-08-24 13:49:49 -0500

    Window Dragging

--- scripts/objects/window.py
+++ scripts/objects/window.py
@@ -2,7 +2,8 @@ from pyray import RED
 
 class Window:
     def __init__(self, position = (0,0), size = (0,0), id = 0, colour = RED):
-        self.position = position
+        self.x = position[0]
+        self.y = position[1]
         self.size = size
         self.id = id
         self.colour = colour
